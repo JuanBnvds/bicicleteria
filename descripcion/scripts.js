5bf426e9dfb91a4ad05b356704d8a77f3c1b3e36
JuanBnvds <ju.benavidesv@duocuc.cl>
2021-06-04 00:43:40 -0400

    creando json y traer datos

---
+++ descripcion/scripts.js
@@ -0,0 +1,37 @@
+console.log('correcto');
+
+document.querySelector('#boton').addEventListener('click',traerDatos);
+
+function traerDatos(){
+    $.ajax({
+        async: false})
+    console.log('dentro de la función')
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET','bicis.json', true);
+
+    xhttp.send();
+
+    xhttp.onreadystatechange = function(){
+
+        if(this.readyState== 4 && this.status == 200){
+
+            //console.log(this.responseText);
+            let datos = JSON.parse(this.responseText);
+            // console.log(datos);
+            let res = document.querySelector('#res')
+            res.innerHTML = '';
+
+            for(let item of datos){
+
+                res.innerHTML += `
+                <tr>
+                    <td>${item.nombreBici}</td>
+                    <td>${item.Descripcion}</td>
+                </tr>
+                `
+                
+            }
+        }
+    }
+}
